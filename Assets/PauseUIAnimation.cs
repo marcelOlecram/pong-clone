a886d05578a0dbb4605e931df40c96e3d0f8bfe4
Marcelo Acosta <marcelo.acosta1994.com>
2018-11-14 20:40:02 -0400

    added animations to UI

---
+++ Assets/PauseUIAnimation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUIAnimation : MonoBehaviour {
+
+    #region variables
+    // public
+    // private
+    [SerializeField]
+    private string fadeOutTriggerName;
+    [SerializeField]
+    private string fadeInTriggerName;
+    //private GameManager gameManager;
+    private Animator _animator;
+    #endregion
+
+    #region UnityMethods
+    private void Awake()
+    {
+        SetInitialReferences();
+    }
+
+    private void OnEnable () {
+        //gameManager.TogglePauseEvent += FadeOut;
+        FadeIn();
+	}
+	
+	private void OnDisable () {
+        //gameManager.TogglePauseEvent -= FadeOut;
+        FadeOut();
+    }
+	#endregion
+	
+	#region MyMethods
+    private void SetInitialReferences()
+    {
+        //gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FadeOut()
+    {
+        _animator.SetTrigger(fadeOutTriggerName);
+    }
+
+    private void FadeIn()
+    {
+        _animator.SetTrigger(fadeInTriggerName);
+    }
+	#endregion
+}
